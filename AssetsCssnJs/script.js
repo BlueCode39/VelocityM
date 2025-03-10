0cd008cefc675438861b144548bf050c0ca58e75
BlueCode39 <144977935+BlueCode39@users.noreply.github.com>
2025-03-10 15:02:05 +0700

    update

--- AssetsCssnJs/script.js
+++ AssetsCssnJs/script.js
@@ -2,3 +2,20 @@ function toggleTheme() {
         var element = document.body;
         element.classList.toggle("dark-mode");
   }
+
+  let nilai = 85;
+
+  if (nilai >= 90) {;
+      console.log("A")
+  } else if (nilai >= 80) {
+      console.log("B");
+  } else if (nilai >= 70) {
+      console.log("C");
+  } else if (nilai >= 60) {
+      console.log("D");
+  } else {
+      console.log("E");
+  }; 
+
+  var user = prompt("Silahkan masukkan username mu.");
+  var pass = prompt("Silahkan masukkan password mu.");
