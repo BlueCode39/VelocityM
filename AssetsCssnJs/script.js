ce73627008a88be7543ca2533bea33a5df37df3f
BlueCode39 <144977935+BlueCode39@users.noreply.github.com>
2025-04-25 09:29:30 +0700

    contact and other shit
    lol

--- AssetsCssnJs/script.js
+++ AssetsCssnJs/script.js
@@ -2,3 +2,22 @@ function toggleTheme() {
         var element = document.body;
         element.classList.toggle("dark-mode");
   }
+
+
+//   Form Functionality
+
+function submitform() {
+      const forename = document.getElementById('udin').value;
+      const surname = document.getElementById('udin2').value;
+      const email = document.getElementById('udin3').value;
+      const textarea = document.getElementById('udin4').value;
+
+      const dataOne = forename && surname ? true : false ;
+      const dataTwo = email && textarea ? true: false ;
+
+      if (dataOne && dataTwo) {
+            alert("Sent successfully!! ₍₍⚞(˶˃ ᵕ ˂˶)⚟⁾⁾");
+      } else {
+            alert("All field must be filled out.. ˙◠˙");
+      }
+}
